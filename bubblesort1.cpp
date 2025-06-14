1f793d5eab57176af6d142e7f440a370efa945fa
Nilotpal Guha <nilotpalguha04@gmail.com>
2025-06-14 12:53:42 +0530

    bubblesort1.cpp

--- bubblesort1.cpp
+++ bubblesort1.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) 
 {
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
             if (arr[j] > arr[j + 1]) 
             {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+        }
+    }
 }
 
 int main() 
@@ -19,7 +21,7 @@ int main()
     cout << "Enter the number of elements: ";
     cin >> n;
 
-    int arr[n]; // Note: For large 'n', consider using dynamic memory instead
+    int arr[n];
 
     cout << "Enter " << n << " elements:\n";
     for (int i = 0; i < n; i++)
