03b2a3b6320a3bd37831eaeeba4688f3cb055cac
Nilotpal Guha <nilotpalguha04@gmail.com>
2025-07-24 14:00:19 +0530

    Add files via upload

---
+++ bubble_sortdesc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter the size of the array: ");
+scanf("%d",&n);
+int ar[n];
+int temp,i,j;
+printf("Enter the array: ");
+for(i=0;i<n;i++){
+scanf("%d",&ar[i]);
+}
+
+for(i=0;i<n-1;i++){
+	for(j=0;j<n-1-i;j++){
+		if(ar[j]<ar[j+1]){
+			temp=ar[j];
+			ar[j]=ar[j+1];
+			ar[j+1]=temp;
+			}
+		}
+	}
+printf("The sorted array: ");
+for(i=0;i<n;i++){
+printf(" %d ",ar[i]);
+}
+return 0;
+}
